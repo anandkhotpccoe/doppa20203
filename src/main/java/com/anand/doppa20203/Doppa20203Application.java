393288563ef7626f435b7f76f6e3f215db061ec9
Anand Khot <anandkhot.pccoe@gmail.com>
2020-06-28 14:19:16 +0000

    Initial Commit

---
+++ src/main/java/com/anand/doppa20203/Doppa20203Application.java
@@ -0,0 +1,13 @@
+package com.anand.doppa20203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doppa20203Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Doppa20203Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/anand/doppa20203/Doppa20203ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.doppa20203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doppa20203ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
